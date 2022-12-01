6805a721a05d709097bc42519d3ff88896df5526
Matiass <matias.brizuela@davinci.edu.ar>
2022-12-01 10:19:59 -0300

    feat: creacion de metodo con getter y setter de ruedas

--- src/garage/Vehiculos/Auto.java
+++ src/garage/Vehiculos/Auto.java
@@ -3,22 +3,25 @@ package garage.Vehiculos;
 public class Auto {
     public int marcaAuto;
     public int cantPuerta;
+    public int cantRuedasAuto;
 
 
 
-
+    public int getCantRuedasAuto() {
+        return cantRuedasAuto;
+    }
+    public void setCantRuedasAuto(int cantRuedasAuto) {
+        this.cantRuedasAuto = cantRuedasAuto;
+    }
     public int getMarcaAuto() {
         return marcaAuto;
     }
-
     public void setMarcaAuto(int marcaAuto) {
         this.marcaAuto = marcaAuto;
     }
-
     public int getCantPuerta() {
         return cantPuerta;
     }
-
     public void setCantPuerta(int cantPuerta) {
         this.cantPuerta = cantPuerta;
     }
--- src/garage/Vehiculos/Moto.java
+++ src/garage/Vehiculos/Moto.java
@@ -3,22 +3,26 @@ package garage.Vehiculos;
 public class Moto {
     public int marcaMoto;
     public int cilindrada;
+    public int cantRuedasMoto;
 
 
 
 
+    public int getCantRuedasMoto() {
+        return cantRuedasMoto;
+    }
+    public void setCantRuedasMoto(int cantRuedasMoto) {
+        this.cantRuedasMoto = cantRuedasMoto;
+    }
     public int getMarcaMoto() {
         return marcaMoto;
     }
-
     public void setMarcaMoto(int marcaMoto) {
         this.marcaMoto = marcaMoto;
     }
-
     public int getCilindrada() {
         return cilindrada;
     }
-
     public void setCilindrada(int cilindrada) {
         this.cilindrada = cilindrada;
     }
