a49524151ec5c1708aa9c49b42f1e2cf0bab9066
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2022-12-03 17:58:40 -0300

    feat: arraylist en llave

--- src/mundial/Llave.java
+++ src/mundial/Llave.java
@@ -1,4 +1,9 @@
 package mundial;
 
-public class Llave {
+import java.util.ArrayList;
+
+public class Llave extends EtapaMundial{
+    public ArrayList<Equipo> getEquiposQueAvanzan() {
+        return null;
+    }
 }
