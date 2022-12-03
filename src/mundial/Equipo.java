0c9370bc9d1080b0d73fe947b3e0c225faf4b0ea
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2022-12-03 17:59:56 -0300

    feat: getter and setters, constructores y atributos, arraylist de etapa mundial y equipo

--- src/mundial/Equipo.java
+++ src/mundial/Equipo.java
@@ -1,4 +1,21 @@
 package mundial;
 
-public class Equipo {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Equipo extends Partido{
+    private String nombre;
+    private ArrayList <Partido> partidosJugados;
+
+    Equipo(Date fecha, Equipo local, Equipo visitante) {
+        super(fecha, local, visitante);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- src/mundial/EtapaMundial.java
+++ src/mundial/EtapaMundial.java
@@ -1,4 +1,24 @@
 package mundial;
 
+import java.util.ArrayList;
+
 public class EtapaMundial {
+    private String descripcionEtapa;
+    private ArrayList <Partido> partidos;
+
+    EtapaMundial(){
+    }
+
+    public String getDescripcionEtapa() {
+        return descripcionEtapa;
+    }
+    public void addPartido (Partido partido){
+
+    }
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+    public ArrayList<Equipo> getEquiposQueAvanzan(){
+        return null;
+    }
 }
