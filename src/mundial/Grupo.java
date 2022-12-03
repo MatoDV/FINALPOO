1e8f78c747977f5c770ba3a204a161d2389597f6
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2022-12-03 17:59:10 -0300

    feat: arraylist de grupo

--- src/mundial/Grupo.java
+++ src/mundial/Grupo.java
@@ -1,4 +1,9 @@
 package mundial;
 
-public class Grupo {
+import java.util.ArrayList;
+
+public class Grupo extends EtapaMundial{
+    public ArrayList<Equipo> getEquiposQueAvanzan() {
+        return null;
+    }
 }
