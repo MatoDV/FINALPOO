5f4b9f3207a88764b805fac4bf20b507d1e7c1ac
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2022-12-03 17:57:41 -0300

    feat: setters and getters constructores y sus atributos

--- src/mundial/Resultado.java
+++ src/mundial/Resultado.java
@@ -1,4 +1,34 @@
 package mundial;
 
-public class Resultado {
+import java.util.Date;
+
+public class Resultado extends Partido{
+    private int golesLocal;
+    private int golesVisitante;
+
+    Resultado(Date fecha, Equipo local, Equipo visitante) {
+        super(fecha, local, visitante);
+    }
+
+    public int getGolesLocal() {
+        return golesLocal;
+    }
+
+    public void setGolesLocal(int golesLocal) {
+        this.golesLocal = golesLocal;
+    }
+
+    public int getGolesVisitante() {
+        return golesVisitante;
+    }
+
+    public void setGolesVisitante(int golesVisitante) {
+        this.golesVisitante = golesVisitante;
+    }
+    public boolean ganoLocal(){
+        return false;
+    }
+    public boolean empate(){
+        return false;
+    }
 }
