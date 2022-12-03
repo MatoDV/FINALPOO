49dff6d073acafad400964811c41b13bb17ce62b
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2022-12-03 17:58:16 -0300

    feat: getter and setter, atributos y constructor de partido

--- src/mundial/Partido.java
+++ src/mundial/Partido.java
@@ -1,4 +1,25 @@
 package mundial;
 
-public class Partido {
+import java.util.Date;
+
+public class Partido extends EtapaMundial{
+    private Date fecha;
+    private Equipo local;
+    private Equipo visitante;
+    private Resultado resultado;
+
+    Partido(Date fecha,Equipo local, Equipo visitante){
+    }
+
+    public Equipo getLocal() {
+        return local;
+    }
+
+    public Equipo getVisitante() {
+        return visitante;
+    }
+
+    public Resultado getResultado() {
+        return resultado;
+    }
 }
