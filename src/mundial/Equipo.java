e70d62880f796ce8bf97a4a088044fc122e82e15
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-03-02 19:50:11 -0300

    feat: add puntos and sumarpuntos

--- src/mundial/Equipo.java
+++ src/mundial/Equipo.java
@@ -6,16 +6,33 @@ import java.util.Date;
 public class Equipo extends Partido{
     private String nombre;
     private ArrayList <Partido> partidosJugados;
+    private int puntos;
+    public int sumarpuntos;
 
     Equipo(Date fecha, Equipo local, Equipo visitante) {
         super(fecha, local, visitante);
     }
-
     public String getNombre() {
         return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getSumarpuntos() {
+        return sumarpuntos;
+    }
+
+    public void setSumarpuntos(int sumarpuntos) {
+        this.sumarpuntos = sumarpuntos;
     }
 }
