70929422346d17093e546cc2c16a28f3ca9491a9
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-03-02 20:06:03 -0300

    feat: add arraylists

--- src/mundial/EtapaMundial.java
+++ src/mundial/EtapaMundial.java
@@ -4,21 +4,29 @@ import java.util.ArrayList;
 
 public class EtapaMundial {
     private String descripcionEtapa;
+
     private ArrayList <Partido> partidos;
-
+    public ArrayList<Equipo>equiposQueAvanzan;
     EtapaMundial(){
+        this.partidos=new ArrayList<Partido>();
+        this.equiposQueAvanzan=new ArrayList<Equipo>();
     }
 
-    public String getDescripcionEtapa() {
-        return descripcionEtapa;
-    }
-    public void addPartido (Partido partido){
-
+    public ArrayList<Partido> addPartido (Partido partidos){
+        this.partidos.add(partidos);
+        return this.partidos;
     }
     public ArrayList<Partido> getPartidos() {
         return partidos;
     }
     public ArrayList<Equipo> getEquiposQueAvanzan(){
         return null;
+    }
+    public String getDescripcionEtapa() {
+        return descripcionEtapa;
+    }
+
+    public void setDescripcionEtapa(String descripcionEtapa) {
+        this.descripcionEtapa = descripcionEtapa;
     }
 }
