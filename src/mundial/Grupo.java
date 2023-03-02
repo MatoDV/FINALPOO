06bf6a7ffe356f4e27ce1322be280fe94a365ec8
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-03-02 20:45:26 -0300

    feat: add fors to add puntos for the teams and add to equiposqueavanzan if the teams have six points

--- src/mundial/Grupo.java
+++ src/mundial/Grupo.java
@@ -1,9 +1,28 @@
 package mundial;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Grupo extends EtapaMundial{
+    public void generarPartidos(ArrayList<Equipo>equipos){
+        for (int i=0;i<equipos.size();i++){
+            for (int k=i+1;k<equipos.size();k++){
+                getPartidos().add(new Partido(equipos.get(i),equipos.get(k),new Date()));
+            }
+        }
+    }
+    @Override
     public ArrayList<Equipo> getEquiposQueAvanzan() {
-        return null;
+        for (int i = 0; i <getPartidos().size() ; i++) {
+            if (getPartidos().get(i).getResultado().ganoLocal()){
+                getPartidos().get(i).getLocal().sumarpuntos();
+            }else getPartidos().get(i).getVisitante().sumarpuntos();
+            if (getPartidos().get(i).getLocal().getPuntos()>=6){
+                this.equiposQueAvanzan.add(getPartidos().get(i).getLocal());
+            } else if (getPartidos().get(i).getVisitante().getPuntos()>=6) {
+                this.equiposQueAvanzan.add(getPartidos().get(i).getVisitante());
+            }
+        }
+        return equiposQueAvanzan;
     }
 }
