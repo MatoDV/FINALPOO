07e5c690326b64552f283f211ba30bb5a56940b8
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-03-02 21:14:43 -0300

    feat: rebuild arraylist adding for to fill equiposqueavanzan

--- src/mundial/Llave.java
+++ src/mundial/Llave.java
@@ -1,9 +1,21 @@
 package mundial;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Llave extends EtapaMundial{
+    public void generarPartido(ArrayList<Equipo>equipos){
+        for (int i=0;i<equipos.size()-1;i+=2){
+            getPartidos().add(new Partido(equipos.get(i),equipos.get(i+1),new Date()));
+        }
+    }
+   @Override
     public ArrayList<Equipo> getEquiposQueAvanzan() {
-        return null;
+        for (int i=0;i<getPartidos().size();i++){
+            if (getPartidos().get(i).getResultado().ganoLocal()){
+                this.equiposQueAvanzan.add(getPartidos().get(i).getLocal());
+            }else this.equiposQueAvanzan.add(getPartidos().get(i).getVisitante());
+        }
+        return equiposQueAvanzan;
     }
 }
