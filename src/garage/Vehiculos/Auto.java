1295130e82e8606d6f9023213421f167021f0a39
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-03-05 14:30:49 -0300

    feat: rebuild class auto to simplifying the code

--- src/garage/Vehiculos/Auto.java
+++ src/garage/Vehiculos/Auto.java
@@ -2,34 +2,16 @@ package garage.Vehiculos;
 
 public class Auto extends Vehiculo{
     public int cantPuerta;
-    public int cantRuedasAuto;
 
-    Auto(){
-    }
-    Auto(String marca){
-        this.setMarca(marca);
-    }
-    Auto(String marca,int cantPuerta){
-        this.setMarca(marca);
-        this.setCantPuerta(cantPuerta);
-    }
-    public Auto(String marca,int cantPuerta, int cantRuedasAuto){
-        this.setMarca(marca);
-        this.setCantPuerta(cantPuerta);
-        this.setCantRuedasAuto(cantRuedasAuto);
+    public Auto(String marca,int cantPuerta,int kilometraje){
+        super(marca,4,kilometraje);
+        this.cantPuerta=cantPuerta;
     }
 
-    public int getCantRuedasAuto() {
-        return cantRuedasAuto;
-    }
-    public void setCantRuedasAuto(int cantRuedasAuto) {
-        this.cantRuedasAuto = cantRuedasAuto;
-    }
     public int getCantPuerta() {
         return cantPuerta;
     }
     public void setCantPuerta(int cantPuerta) {
         this.cantPuerta = cantPuerta;
     }
-
 }
