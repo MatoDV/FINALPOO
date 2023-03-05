38d60834deaf81872c88ac22d63e520621cd5cf2
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-03-05 14:31:17 -0300

    feat: add objects and print

--- src/garage/SistemaGarage/garageTest.java
+++ src/garage/SistemaGarage/garageTest.java
@@ -2,12 +2,25 @@ package garage.SistemaGarage;
 
 import garage.Vehiculos.Auto;
 import garage.Vehiculos.Moto;
+import garage.Vehiculos.Vehiculo;
 
 public class GarageTest {
     public static void main(String[] args) {
-        Auto vw = new Auto("vw",4,4);
-        Moto bmw = new Moto("bmw",4,2);
+        Garage garage = new Garage(10, 400);
 
-        //if(capacidadActual=capacidadMax){System.out.println("Ya no hay mas espacio :(");}
+        Auto vw = new Auto("vw", 4, 8000);
+        Moto bmw = new Moto("bmw", 4, 2500);
+
+        garage.añadirVehiculo(vw);
+        garage.añadirVehiculo(bmw);
+
+        System.out.println("Total de vehiculos: " + garage.getTotalVehiculos());
+        System.out.println("Precio total de ruedas: " + garage.getTotalPrecioRuedas());
+        System.out.println("Kilometraje promedio: " + garage.getKilometrajeTotal());
+
+
+        for (Vehiculo vehiculo : garage.vehiculos) {
+            System.out.println("Marca: " + vehiculo.getMarca() + ", Kilometraje: " + vehiculo.getKilometraje());
+        }
     }
 }
