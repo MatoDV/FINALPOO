97fc5a3409ef9387b7cffb5addb34fe3446f9723
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-03-05 14:30:30 -0300

    feat: rebuild class moto simplifying all of the code

--- src/garage/Vehiculos/Moto.java
+++ src/garage/Vehiculos/Moto.java
@@ -2,29 +2,10 @@ package garage.Vehiculos;
 
 public class Moto extends Vehiculo {
     public int cilindrada;
-    public int cantRuedasMoto;
 
-    Moto(){
-    }
-    Moto(String marca){
-        this.setMarca(marca);
-    }
-    Moto(String marca,int cilindrada){
-        this.setMarca(marca);
-        this.setCilindrada(cilindrada);
-    }
-    public Moto(String marcaMoto, int cilindrada, int cantRuedasMoto){
-        this.setMarca(marca);
-        this.setCilindrada(cilindrada);
-        this.setCantRuedasMoto(cantRuedasMoto);
-    }
-
-
-    public int getCantRuedasMoto() {
-        return cantRuedasMoto;
-    }
-    public void setCantRuedasMoto(int cantRuedasMoto) {
-        this.cantRuedasMoto = cantRuedasMoto;
+    public Moto(String marca,int cilindrada,int kilometraje){
+        super(marca,2,kilometraje);
+        this.cilindrada=cilindrada;
     }
     public int getCilindrada() {
         return cilindrada;
@@ -32,5 +13,4 @@ public class Moto extends Vehiculo {
     public void setCilindrada(int cilindrada) {
         this.cilindrada = cilindrada;
     }
-
 }
