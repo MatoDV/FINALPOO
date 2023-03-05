b93bbed6517f0349b100686bd2d05b4d290765d1
Matias Brizuela <matias.brizuela@davinci.edu.ar>
2023-03-05 14:29:29 -0300

    feat: add cantRuedas,kilometraje setters and getters

--- src/garage/Vehiculos/Vehiculo.java
+++ src/garage/Vehiculos/Vehiculo.java
@@ -2,13 +2,32 @@ package garage.Vehiculos;
 
 public class Vehiculo {
     public String marca;
+    public int cantRuedas;
+    public int kilometraje;
+    public Vehiculo(String marca,int cantRuedas,int kilometraje){
+        this.marca=marca;
+        this.cantRuedas=cantRuedas;
+        this.kilometraje=kilometraje;
+    }
 
+    public int getCantRuedas() {
+        return cantRuedas;
+    }
 
+    public void setCantRuedas(int cantRuedas) {
+        this.cantRuedas = cantRuedas;
+    }
     public String getMarca() {
         return marca;
     }
     public void setMarca(String marca) {
         this.marca = marca;
     }
+    public int getKilometraje() {
+        return kilometraje;
+    }
 
+    public void setKilometraje(int kilometraje) {
+        this.kilometraje = kilometraje;
+    }
 }
